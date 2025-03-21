f93579db71c28f464f6b7b8a2fab677e1302eded
PRANAV_REDDY <reddysaipranav6@gmail.com>
2025-03-21 21:50:07 +0530

    Updated_API

--- main.py
+++ main.py
@@ -31,4 +31,4 @@ async def predict(file: UploadFile = File(...)):
     return {"filename": file.filename, "prediction": result}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="localhost", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
